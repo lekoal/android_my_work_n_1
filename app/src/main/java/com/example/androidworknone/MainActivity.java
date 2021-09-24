767e1603dbccf85e74902e57ee29f6e11820ed21
lekoal <coreglus@gmail.com>
2021-09-24 11:32:24 +0300

    Created the second layout an added there EditText elements

--- app/src/main/java/com/example/androidworknone/MainActivity.java
+++ app/src/main/java/com/example/androidworknone/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main); // активация макета задания 1
+        setContentView(R.layout.activity_main_2); // активация макета задания 2
     }
 }
